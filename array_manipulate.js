f332b867f72185eda10d458d7fa4dcb73b61bf29
hhpatrickchen <hhpatrickchen@gmail.com>
2017-09-09 16:35:51 +0800

    sparse array

--- array_manipulate.js
+++ array_manipulate.js
@@ -11,3 +11,25 @@ array.swap(2,4);
 console.log(array)
 
 
+//Sparse Arrays 
+var input = "4\naba\nbaba\naba\nxzxb\n3\naba\nxzxb\nab";
+function processData(input) {
+    var arr = input.split('\n');
+    var data_count = parseInt(arr[0]) ;
+    var data_arr = arr.slice(1,1+ data_count);
+    var test_count = arr[1+ data_count];
+    var test_arr = arr.slice(1+ data_count+1);
+
+    var count = 0;
+    for(var i = 0; i < test_count; i++){
+        count = 0;
+        for(var j = 0; j <data_count;j++){
+            if(test_arr[i] == data_arr[j]){
+                count++;
+            }
+        }
+        console.log(count);
+    }
+
+} 
+ processData(input);
