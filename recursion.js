3a8d7972fbafe48befcb23c0c2dfced60df74472
hhpatrickchen <hhpatrickchen@gmail.com>
2017-09-09 16:32:32 +0800

    gcd,  steps issue

--- recursion.js
+++ recursion.js
@@ -1,3 +1,4 @@
+//power sum
 function powersum(value, pow, num){
     var total = value - Math.pow(num, pow);
 
@@ -6,3 +7,24 @@ function powersum(value, pow, num){
 
     return powersum(total, pow, num+1) + powersum(value, pow, num +1);
 }
+
+//gcd
+//var a = 24, b = 54;
+function gcd( a,  b)
+{
+    // 令 a 比 b 大，比較容易思考。
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+//5 steps problem
+function steps(n){
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+
+    return steps(n-1)+ steps(n-2);
+}
+
+console.log(steps(12));
